a36e34834928948113a02b9508eb1674f6787eec
Subhrajit Roy <subhrajit.roy@gmail.com>
2020-03-16 11:30:37 +0800

    Adding frontend files to Github Pages

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config();
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
@@ -13,7 +15,21 @@ module.exports = {
                port: 8545,
                network_id: "4224",
                gas: 4700000
-          }
+          },
+          rinkeby: {
+               host: "localhost",
+               port: 8545,
+               network_id: 4, // Rinkeby test network
+               gas: 4700000
+           },
+           ropsten: {
+               provider: function () {
+                   return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY);
+               },
+               network_id: 3,
+               gas: 4500000,
+               gasPrice: 10000000000
+            }
      },
      compilers: {
           solc: {
